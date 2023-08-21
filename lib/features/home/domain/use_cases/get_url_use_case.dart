f5d8fa5d7db2655251cb98349468ad85c253f4c4
farhat <farhatbaymuradov3@gmail.com>
2023-08-21 18:16:27 +0500

    ios firebase configurations done

--- lib/features/home/domain/use_cases/get_url_use_case.dart
+++ lib/features/home/domain/use_cases/get_url_use_case.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:test1377/core/errors/errors.dart';
 import 'package:test1377/features/home/domain/repositories/get_url_repo.dart';
@@ -21,33 +23,39 @@ class GetUrlUseCase {
 
   checkIsEmu() async {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    final em = await deviceInfo.androidInfo;
-    var phoneModel = em.model;
-    var buildProduct = em.product;
-    var buildHardware = em.hardware;
-    var result = (em.fingerprint.startsWith("generic") ||
-        phoneModel.contains("google_sdk") ||
-        phoneModel.contains("droid4x") ||
-        phoneModel.contains("Emulator") ||
-        phoneModel.contains("Android SDK built for x86") ||
-        em.manufacturer.contains("Genymotion") ||
-        buildHardware == "goldfish" ||
-        buildHardware == "vbox86" ||
-        buildProduct == "sdk" ||
-        buildProduct == "google_sdk" ||
-        buildProduct == "sdk_x86" ||
-        buildProduct == "vbox86p" ||
-        em.brand.contains('google') ||
-        em.board.toLowerCase().contains("nox") ||
-        em.bootloader.toLowerCase().contains("nox") ||
-        buildHardware.toLowerCase().contains("nox") ||
-        !em.isPhysicalDevice ||
-        buildProduct.toLowerCase().contains("nox"));
-    if (result) return true;
-    result = result ||
-        (em.brand.startsWith("generic") && em.device.startsWith("generic"));
-    if (result) return true;
-    result = result || ("google_sdk" == buildProduct);
-    return result;
+    if (Platform.isAndroid) {
+      final em = await deviceInfo.androidInfo;
+      var phoneModel = em.model;
+      var buildProduct = em.product;
+      var buildHardware = em.hardware;
+      var result = (em.fingerprint.startsWith("generic") ||
+          phoneModel.contains("google_sdk") ||
+          phoneModel.contains("droid4x") ||
+          phoneModel.contains("Emulator") ||
+          phoneModel.contains("Android SDK built for x86") ||
+          em.manufacturer.contains("Genymotion") ||
+          buildHardware == "goldfish" ||
+          buildHardware == "vbox86" ||
+          buildProduct == "sdk" ||
+          buildProduct == "google_sdk" ||
+          buildProduct == "sdk_x86" ||
+          buildProduct == "vbox86p" ||
+          em.brand.contains('google') ||
+          em.board.toLowerCase().contains("nox") ||
+          em.bootloader.toLowerCase().contains("nox") ||
+          buildHardware.toLowerCase().contains("nox") ||
+          !em.isPhysicalDevice ||
+          buildProduct.toLowerCase().contains("nox"));
+      if (result) return true;
+      result = result ||
+          (em.brand.startsWith("generic") && em.device.startsWith("generic"));
+      if (result) return true;
+      result = result || ("google_sdk" == buildProduct);
+      return result;
+    } else if (Platform.isIOS) {
+      final em = await deviceInfo.iosInfo;
+      return !em.isPhysicalDevice;
+    }
+    return false;
   }
 }
