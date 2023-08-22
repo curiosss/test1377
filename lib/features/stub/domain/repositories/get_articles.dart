2bf6d7aed1df48d7719b2e407254ee7658d7d5e5
Farhat Baymuradov <farhatbaymuradov3@gmail.com>
2023-08-22 13:43:21 +0500

    junk removed

--- lib/features/stub/domain/repositories/get_articles.dart
+++
@@ -1,7 +0,0 @@
-import 'package:test1377/core/errors/errors.dart';
-
-import '../entities/article.dart';
-
-abstract class ArticlesRepo {
-  Future<(KError? err, List<Article> articles)> getArticles();
-}
